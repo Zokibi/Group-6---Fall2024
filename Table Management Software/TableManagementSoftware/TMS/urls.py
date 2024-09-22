b9b969333ec9992f870ca301eb31fef31b131082
AnthonyTheRedguard <ajgilliam@my.waketech.edu>
2024-09-21 23:31:35 -0400

    Fixed the Login Page url

--- Table Management Software/TableManagementSoftware/TMS/urls.py
+++ Table Management Software/TableManagementSoftware/TMS/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"), # will call the home function in views
-    path("signin/", views.sign_in_view, name="signin"), # will call the sign_in_view function in views
+    path("login/", views.sign_in_view, name="login"), # will call the sign_in_view function in views
     path("signup/", views.sign_up_view, name="signup"), # will call the sign_up_view function in views
     path("about/", views.about_view, name="about")
 ]
