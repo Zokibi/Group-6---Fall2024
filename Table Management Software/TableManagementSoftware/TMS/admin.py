16471a55cefc3447f5b47acfacf61e94fa33b94d
Isaiah Dillard <isaiahtdillard@gmail.com>
2024-09-29 17:24:08 -0400

    Add tables to database
    Added a few tables to the database

--- Table Management Software/TableManagementSoftware/TMS/admin.py
+++ Table Management Software/TableManagementSoftware/TMS/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import EmployeeClockin
+from .models import MealOrder, Employees, Tables
 
 # Register your models here.
-admin.site.register(EmployeeClockin)
+admin.site.register(MealOrder)
+admin.site.register(Employees)
+admin.site.register(Tables)
--- Table Management Software/TableManagementSoftware/TMS/models.py
+++ Table Management Software/TableManagementSoftware/TMS/models.py
@@ -2,7 +2,38 @@ from django.db import models
 
 # Create your database models here.
 
-class EmployeeClockin(models.Model):
-    Server = models.CharField(max_length=200)
-    clockin = models.BooleanField(default=False)
+
+class Employees(models.Model):
+    employeeList = {
+        "1": "Charne Robinson",
+        "2": "Jacob Sparks",
+        "3": "Daryna Kozlova",
+        "4": "Isaiah Dillard",
+        "5": "Anthony Gilliam",
+    } # list of all the employees for selection
+    employeeSelection = models.CharField(max_length=10, choices=employeeList)
     
+
+class MealOrder(models.Model):
+    menuOptions = {
+        "1": "Cheese Burger",
+        "2": "Double Cheese Burger",
+        "3": "Steak Burrito",
+        "4": "Chicken Burrito",
+        "5": "Pepperoni Pizza",
+        "6": "Grilled Chicken Salad",
+    }
+    menuSelection = models.CharField(max_length=1, choices=menuOptions)
+    
+class Tables(models.Model):
+    tableNumbers = {
+        "1": "1- 4 seats",
+        "2": "2- 4 seats",
+        "3": "3- 4 seats",
+        "4": "4- 6 seats",
+        "5": "5- 6 seats",
+        "6": "6- 6 seats",
+        "7": "7- 8 seats",
+        "8": "8- 8 seats",
+    }
+    tableSelection = models.CharField(max_length=1, choices=tableNumbers)
