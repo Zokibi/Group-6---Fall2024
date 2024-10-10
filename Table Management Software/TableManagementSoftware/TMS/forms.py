5678c259b02b0d78d12eedeee01aa83c8ab7270b
AnthonyTheRedguard <ajgilliam@my.waketech.edu>
2024-10-10 17:05:40 -0400

    Fixed Scaling of the Login and Sign In Pages

--- Table Management Software/TableManagementSoftware/TMS/forms.py
+++ Table Management Software/TableManagementSoftware/TMS/forms.py
@@ -8,26 +8,36 @@ class CreateUserForm(UserCreationForm):
             self.fields['username'].widget.attrs.update({
                  'placeholder': ' Enter Username',
                  'class': 'form-input',
-                 'style': 'height: 60px; width: 600px; background-color: transparent; border: 1px white solid; border-radius: 5px; font-size: 20px; color: white'
+                 'style': 'background-color: transparent; border: 1px white solid; border-radius: 5px; font-size: 20px; color: white'
             })
             self.fields['email'].widget.attrs.update({
                  'placeholder': ' Enter Email',
                  'class': 'form-input',
-                 'style': 'height: 60px; width: 600px; background-color: transparent; border: 1px white solid; border-radius: 5px; font-size: 20px; color: white'
+                 'style': 'background-color: transparent; border: 1px white solid; border-radius: 5px; font-size: 20px; color: white'
+            })
+            self.fields['first_name'].widget.attrs.update({
+                 'placeholder': ' Enter First Name',
+                 'style': 'background-color: transparent; border: 1px white solid; border-radius: 5px; font-size: 20px; color: white',
+                 'class': 'name-input',
+            })
+            self.fields['last_name'].widget.attrs.update({
+                 'placeholder': ' Enter Last Name',
+                 'style': 'background-color: transparent; border: 1px white solid; border-radius: 5px; font-size: 20px; color: white',
+                 'class': 'name-input',
             })
             self.fields['password1'].widget.attrs.update({
                  'placeholder': ' Enter Password',
                  'class': 'form-input',
-                 'style': 'height: 60px; width: 600px; background-color: transparent; border: 1px white solid; border-radius: 5px; font-size: 20px; color: white'
+                 'style': 'background-color: transparent; border: 1px white solid; border-radius: 5px; font-size: 20px; color: white'
             })
             self.fields['password2'].widget.attrs.update({
                  'placeholder': ' Confirm Password',
                  'class': 'form-input',
-                 'style': 'height: 60px; width: 600px; background-color: transparent; border: 1px white solid; border-radius: 5px; font-size: 20px; color: white'
+                 'style': 'background-color: transparent; border: 1px white solid; border-radius: 5px; font-size: 20px; color: white'
             })
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name','password1', 'password2']
 
         
 
