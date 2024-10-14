f397fac453454171e0d4c370bdf7bd0040e972ce
AnthonyTheRedguard <ajgilliam@my.waketech.edu>
2024-10-13 20:48:12 -0400

    Edited models.py

--- Table Management Software/TableManagementSoftware/TMS/models.py
+++ Table Management Software/TableManagementSoftware/TMS/models.py
@@ -6,3 +6,29 @@ class EmployeeClockin(models.Model):
     Server = models.CharField(max_length=200)
     clockin = models.BooleanField(default=False)
     
+class User(models.Model):
+    userID = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    role = models.CharField(max_length=30)
+
+class Table(models.Model):
+    tableID = models.CharField(primary_key=True, max_length=30)
+    chairs = models.IntegerField
+    table_status = models.CharField(max_length=30)
+
+class Order(models.Model):
+    orderID = models.CharField(primary_key=True, max_length=30)
+    tableID = models.CharField(max_length=30)
+    order_status = models.CharField(max_length=30)
+
+class Item(models.Model):
+    itemID = models.CharField(primary_key=True, max_length=30)
+    itemName = models.CharField(max_length=30)
+    price = models.FloatField
+
+
+
+
+
