f71d0867c4d9da9f576fd94d3f8d59042521afe4
AnthonyTheRedguard <ajgilliam@my.waketech.edu>
2024-10-16 20:19:33 -0400

    Finished the Login and Signup
    It took way too long, but it actually works this time.

--- Table Management Software/TableManagementSoftware/TMS/forms.py
+++ Table Management Software/TableManagementSoftware/TMS/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from typing import Any
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from django.contrib.auth.models import User
 
@@ -39,6 +40,21 @@ class CreateUserForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'first_name', 'last_name','password1', 'password2']
 
-        
+class CreateLoginForm(AuthenticationForm):
+     def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['username'].widget.attrs.update({
+                 'placeholder': ' Enter Username',
+                 'class': 'form-input',
+                 'style': 'background-color: transparent; border: 1px white solid; border-radius: 5px; font-size: 20px; color: white'
+            })
+            self.fields['password'].widget.attrs.update({
+                 'placeholder': ' Enter Password',
+                 'class': 'form-input',
+                 'style': 'background-color: transparent; border: 1px white solid; border-radius: 5px; font-size: 20px; color: white'
+            })
+     class Meta:
+           model = User
+           fields = ['username' , 'password']
 
         
--- Table Management Software/TableManagementSoftware/TMS/views.py
+++ Table Management Software/TableManagementSoftware/TMS/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render, HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import login
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-from .forms import CreateUserForm
+from .forms import CreateUserForm, CreateLoginForm
 
 # Create your views here.
 def home(request):
@@ -16,23 +13,13 @@ def home(request):
 
 def login_view(request):
     if request.method == 'POST':
-        # Get username and password from POST request
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        # Authenticate the user
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            # Log the user in and redirect to the home page (or wherever you like)
-            login(request, user)
-            return redirect('home')  # 'home' should be the name of the URL route for the home page
-        else:
-            # Invalid credentials, show an error message
-            messages.error(request, 'Invalid username or password.')
-
-    # If GET request or failed login, re-render the login page
-    return render(request, 'login.html')  # 'login.html' is your login template
+        form = CreateLoginForm(data = request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("about")
+    else:
+        form = CreateLoginForm()
+    return render(request, 'login.html', {'form': form})
 
 def signup_view(request):
     form = CreateUserForm()
@@ -40,13 +27,13 @@ def signup_view(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             user = form.cleaned_data.get('username')
             messages.success(request, 'Account created for user ' + user)
 
         # # Log the user in and redirect to home page
-        # login(request, user)
-        # return redirect('home')  # Adjust this to redirect to your desired page
+        
+        return redirect('about')  # Adjust this to redirect to your desired page
 
     return render(request, 'sign_up.html', {'form':form})  # Render sign-up page on GET request 
 
