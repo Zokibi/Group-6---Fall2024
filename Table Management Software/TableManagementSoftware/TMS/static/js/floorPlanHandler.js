4af6030b0278e2eef65483b4be726bd1026d62f7
AnthonyTheRedguard <ajgilliam@my.waketech.edu>
2024-12-06 11:33:52 -0500

    final-version

--- Table Management Software/TableManagementSoftware/TMS/static/js/floorPlanHandler.js
+++ Table Management Software/TableManagementSoftware/TMS/static/js/floorPlanHandler.js
@@ -12,29 +12,25 @@ window.addEventListener('load', function() {
 
     // Parsing JSON containing table data from database
     var tables_json = JSON.parse(document.getElementById('tables_json').textContent);
-    var restaurants_json = JSON.parse(this.document.getElementById('restaurants_json').textContent);
-    var employees_json = JSON.parse(document.getElementById('employees_json').textContent);
     var users_json = JSON.parse(this.document.getElementById('users_json').textContent);
 
     // Initializing empty arrays to hold table data
     var tables = []
     var names = []
+    var name_id = {}
 
     console.log('Tables');
     console.log(tables);
-    console.log('Restaurants');
-    console.log(restaurants_json);
-    console.log('Employees');
-    console.log(employees_json);
-    console.log('Users JSON');
-    console.log(users_json);
- 
+    console.log('Tables JSON');
+    console.log(tables_json);
+    
     users_json.forEach((user) => {
         names.push(user.first_name + ' ' + user.last_name)
-    })
 
-    console.log('Names');
-    console.log(names)
+        names.forEach((name) => {
+            name_id[name] = user.id;
+        })
+    })
 
     var iterateX = 0
 
@@ -42,10 +38,12 @@ window.addEventListener('load', function() {
     var circle_y = 120
     var rect_y = 80
 
+    var json_index = 0;
+
     tables_json.forEach((element) => {
         if (element.shape =='circle'){
         tables.push({
-            id: element.tableID, type: element.shape, x:x[iterateX], y:circle_y, radius: 40, status: element.table_status, seats: element.seats, guests: element.guests, waiter: choices = names, elapsedTime: 0, startTime: 0, isRunning: false})
+            id: element.tableID, type: element.shape, x:x[iterateX], y:circle_y, radius: 40, status: element.table_status, seats: element.seats, guests: element.guests, waiter: choices = names, elapsedTime: 0, startTime: 0, isRunning: false, index: json_index})
 
             if (x[iterateX] == 100){
                 iterateX += 1;
@@ -57,33 +55,32 @@ window.addEventListener('load', function() {
 
         }else if (element.shape == 'rect'){
             tables.push({
-                id: element.tableID, type: element.shape, x:300, y:rect_y, width: 120, height: 60, status: element.table_status, seats: element.seats, guests: element.guests, waiter: choices = names, elapsedTime: 0, startTime: 0, isRunning: false})
+                id: element.tableID, type: element.shape, x:300, y:rect_y, width: 120, height: 60, status: element.table_status, seats: element.seats, guests: element.guests, waiter: choices = names, elapsedTime: 0, startTime: 0, isRunning: false, index: json_index})
             
             rect_y += 100;
         }
-        
-        
+        json_index++;
     });
 
     
     // Added timer-related variables
     /*const tables = [
         // Circular tables arranged in left column (increased radius to 40)
-        { id: 1, type: 'circle', x: 100, y: 100, radius: 40, status: 'occupied', seats: 10, guests: 8, waiter: 'John T.', elapsedTime: 0, startTime: 0, isRunning: false },
-        { id: 2, type: 'circle', x: 100, y: 300, radius: 40, status: 'available', seats: 6, guests: 0, waiter: 'N/A', elapsedTime: 0, startTime: 0, isRunning: false },
-        { id: 3, type: 'circle', x: 100, y: 500, radius: 40, status: 'available', seats: 6, guests: 0, waiter: 'N/A', elapsedTime: 0, startTime: 0, isRunning: false },
+        { id: 1, type: 'circle', x: 100, y: 100, radius: 40, status: 'Occupied', seats: 10, guests: 8, waiter: 'John T.', elapsedTime: 0, startTime: 0, isRunning: false },
+        { id: 2, type: 'circle', x: 100, y: 300, radius: 40, status: 'Available', seats: 6, guests: 0, waiter: 'N/A', elapsedTime: 0, startTime: 0, isRunning: false },
+        { id: 3, type: 'circle', x: 100, y: 500, radius: 40, status: 'Available', seats: 6, guests: 0, waiter: 'N/A', elapsedTime: 0, startTime: 0, isRunning: false },
         
         // Rectangular tables for 4 people in middle column (increased size and added more tables)
-        { id: 4, type: 'rect', x: 300, y: 80, width: 120, height: 60, status: 'occupied', seats: 4, guests: 4, waiter: 'Sarah M.',  elapsedTime: 0, startTime: 0, isRunning: false  },
-        { id: 5, type: 'rect', x: 300, y: 200, width: 120, height: 60, status: 'available', seats: 4, guests: 0, waiter: 'N/A',  elapsedTime: 0, startTime: 0, isRunning: false  },
-        { id: 9, type: 'rect', x: 300, y: 320, width: 120, height: 60, status: 'available', seats: 4, guests: 0, waiter: 'N/A',  elapsedTime: 0, startTime: 0, isRunning: false  },
-        { id: 10, type: 'rect', x: 300, y: 440, width: 120, height: 60, status: 'reserved', seats: 4, guests: 0, waiter: 'Lisa R.',  elapsedTime: 0, startTime: 0, isRunning: false  },
-        { id: 11, type: 'rect', x: 300, y: 560, width: 120, height: 60, status: 'available', seats: 4, guests: 0, waiter: 'N/A',  elapsedTime: 0, startTime: 0, isRunning: false  },
+        { id: 4, type: 'rect', x: 300, y: 80, width: 120, height: 60, status: 'Occupied', seats: 4, guests: 4, waiter: 'Sarah M.',  elapsedTime: 0, startTime: 0, isRunning: false  },
+        { id: 5, type: 'rect', x: 300, y: 200, width: 120, height: 60, status: 'Available', seats: 4, guests: 0, waiter: 'N/A',  elapsedTime: 0, startTime: 0, isRunning: false  },
+        { id: 9, type: 'rect', x: 300, y: 320, width: 120, height: 60, status: 'Available', seats: 4, guests: 0, waiter: 'N/A',  elapsedTime: 0, startTime: 0, isRunning: false  },
+        { id: 10, type: 'rect', x: 300, y: 440, width: 120, height: 60, status: 'Reserved', seats: 4, guests: 0, waiter: 'Lisa R.',  elapsedTime: 0, startTime: 0, isRunning: false  },
+        { id: 11, type: 'rect', x: 300, y: 560, width: 120, height: 60, status: 'Available', seats: 4, guests: 0, waiter: 'N/A',  elapsedTime: 0, startTime: 0, isRunning: false  },
         
         // Small rectangular tables for 2 people in right column (slightly increased size)
-        { id: 6, type: 'rect', x: 550, y: 100, width: 50, height: 70, status: 'available', seats: 2, guests: 0, waiter: 'N/A',  elapsedTime: 0, startTime: 0, isRunning: false  },
-        { id: 7, type: 'rect', x: 550, y: 300, width: 50, height: 70, status: 'occupied', seats: 2, guests: 2, waiter: 'Mike P.',  elapsedTime: 0, startTime: 0, isRunning: false  },
-        { id: 8, type: 'rect', x: 550, y: 500, width: 50, height: 70, status: 'available', seats: 2, guests: 0, waiter: 'N/A',  elapsedTime: 0, startTime: 0, isRunning: false }
+        { id: 6, type: 'rect', x: 550, y: 100, width: 50, height: 70, status: 'Available', seats: 2, guests: 0, waiter: 'N/A',  elapsedTime: 0, startTime: 0, isRunning: false  },
+        { id: 7, type: 'rect', x: 550, y: 300, width: 50, height: 70, status: 'Occupied', seats: 2, guests: 2, waiter: 'Mike P.',  elapsedTime: 0, startTime: 0, isRunning: false  },
+        { id: 8, type: 'rect', x: 550, y: 500, width: 50, height: 70, status: 'Available', seats: 2, guests: 0, waiter: 'N/A',  elapsedTime: 0, startTime: 0, isRunning: false }
     ];*/
 
 
@@ -148,42 +145,49 @@ window.addEventListener('load', function() {
         
         // Create editor form
         editorContent.innerHTML = `
-            <form method="POST" action="{% url 'layout' %}">
-                <div class="form-group">
-                    <label>Status:</label>
-                    <select id="table-status">
-                        <option value="available" ${table.status === 'available' ? 'selected' : ''}>Available</option>
-                        <option value="occupied" ${table.status === 'occupied' ? 'selected' : ''}>Occupied</option>
-                        <option value="reserved" ${table.status === 'reserved' ? 'selected' : ''}>Reserved</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label>Number of Guests:</label>
-                    <input type="number" id="table-guests" value="${table.guests}" min="0" max="${table.seats}">
-                </div>
-                <div class="form-group">
-                    <label>Assigned Waiter:</label>
-                    <select id="table-waiter">
-                    </select>
-                </div>
-                <div class="form-group">
-                    <button onclick="updateTable(${table.id})">Update Table</button>
-                    <button class="cancel" onclick="cancelEdit()">Cancel</button>
-                </div>
-            </form>
+            <div class="form-group">
+                <label>Status:</label>
+                <select id="table-status">
+                    <option value="Available" ${table.status === 'Available' ? 'selected' : ''}>Available</option>
+                    <option value="Occupied" ${table.status === 'Occupied' ? 'selected' : ''}>Occupied</option>
+                    <option value="Reserved" ${table.status === 'Reserved' ? 'selected' : ''}>Reserved</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label>Number of Guests:</label>
+                <select id="table-guests">
+                </select>
+            </div>
+            <div class="form-group">
+                <label>Assigned Waiter:</label>
+                <select id="table-waiter">
+                </select>
+            </div>
+            <div class="form-group">
+                <button onclick="updateTable(${table.id})">Update Table</button>
+                <button class="cancel" onclick="cancelEdit()">Cancel</button>
+            </div>
             <div class="form-group">
                 <button onclick="startTimer(${table.id})">Start Timer</button> 
                 <button class="cancel" onclick="resetTimer(${table.id})">Reset Timer</button>            
             </div>
         `;
-        var select = document.getElementById('table-waiter');
+        var table_waiter = document.getElementById('table-waiter');
 
         names.forEach((name) => {
-            var option = document.createElement("option");
+            let option = document.createElement("option");
             option.textContent = name;
             option.value = name;
-            select.appendChild(option)
+            table_waiter.appendChild(option);
         })
+
+        var table_guests = document.getElementById('table-guests');
+
+        for (let i = 0; i <= table.seats; i++){
+            let option = document.createElement("option");
+            option.textContent = i;
+            table_guests.appendChild(option);
+        } 
     }
 
     // Global functions for the editor
@@ -253,11 +257,34 @@ window.addEventListener('load', function() {
         table.guests = parseInt(document.getElementById('table-guests').value);
         table.waiter = document.getElementById('table-waiter').value;
 
+        /*updated_table = tables.find(t => t.id === tableId);
+
+        console.log('Updated Table');
+        console.log(updated_table);
+
+        updated_table['table_status'] = table.status
+        updated_table['guests'] = table.guests
+        updated_table['employee_id'] = name_id[document.getElementById('table-waiter').value];*/
+
+        target_table = tables_json[table.index];
+
+        target_table['table_status'] = table.status;
+        target_table['guests'] = table.guests;
+        target_table['employee_id'] = name_id[document.getElementById('table-waiter').value];
+
+        tables_json[table.index]['']
+
+        console.log('Updated Table JSON')
+        console.log(tables_json)
+
+
+
+
         // Update shape color
         const shape = selectedShape;
         if (shape) {
-            shape.fill(table.status === 'occupied' ? '#dc2626' : 
-                      table.status === 'reserved' ? '#f59e0b' : '#22c55e');
+            shape.fill(table.status === 'Occupied' ? '#dc2626' : 
+                      table.status === 'Reserved' ? '#f59e0b' : '#22c55e');
             shape.strokeWidth(0);
             layer.draw();
         }
@@ -279,8 +306,8 @@ window.addEventListener('load', function() {
     // Create tables
     tables.forEach(table => {
         let shape;
-        const isOccupied = table.status === 'occupied';
-        const isReserved = table.status === 'reserved';
+        const isOccupied = table.status === 'Occupied';
+        const isReserved = table.status === 'Reserved';
         const fill = isOccupied ? '#dc2626' : 
                     isReserved ? '#f59e0b' : '#22c55e';
 
